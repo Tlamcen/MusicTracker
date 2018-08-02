cb1275e7c1f72124a12a4135c18b1e7592b4d47a
Tlamcen <Tlamcen@users.noreply.github.com>
2018-08-02 20:53:09 +0200

    Read artists value in storage to get followed artists

--- src/pages/artists/artists.ts
+++ src/pages/artists/artists.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NativeStorage } from '@ionic-native/native-storage';
 
 @Component({
   selector: 'page-artists',
@@ -7,8 +8,16 @@ import { NavController } from 'ionic-angular';
 })
 export class ArtistsPage {
 
-  constructor(public navCtrl: NavController) {
+  artists:Array<string> = [];
 
+  constructor(public navCtrl: NavController, private nativeStorage : NativeStorage) {
+
+    this.nativeStorage.getItem('artists').then((value) => {
+      var artists : Array<string> = JSON.parse(value);
+      this.artists = artists;
+    },(error) => {
+      console.log("There is no artists value in storage or plugin is not loaded");
+    });
   }
 
 }
