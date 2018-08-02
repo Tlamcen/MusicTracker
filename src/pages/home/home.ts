96c45c3027a129c6fc266f5d62c4a380c1ab0a7a
Tlamcen <Tlamcen@users.noreply.github.com>
2018-08-02 20:35:46 +0200

    Home page can read native storage to retrieve last releases spotted

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NativeStorage } from '@ionic-native/native-storage';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,29 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  lastReleases:Array<Object> = [];
 
+  constructor(public navCtrl: NavController, private nativeStorage : NativeStorage) {
+    var arrayTest =
+    [
+      {
+          "artistName" : "Falling In Reverse",
+          "releaseName" : "Losing My Mind",
+          "releaseType" : "Single",
+          "releaseDate" : "21/07/18"
+      },
+      {
+        "artistName" : "Bury Tomorrow",
+        "releaseName" : "Black Flame",
+        "releaseType" : "LP",
+        "releaseDate" : "28/07/18"
+      }
+    ];
+    this.nativeStorage.setItem('releases', JSON.stringify(arrayTest));
+    this.nativeStorage.getItem('releases').then((value) => {
+      var lastReleases : Array<Object> = JSON.parse(value);
+      this.lastReleases = lastReleases;
+    });
   }
 
 }
