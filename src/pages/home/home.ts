8aa4de3c5b814b5542fd251e672c18e97cf2b7cb
Tlamcen <Tlamcen@users.noreply.github.com>
2018-08-03 14:01:06 +0200

    Get an access token from spotify API

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { NativeStorage } from '@ionic-native/native-storage';
+import * as $ from "jquery";
 
 @Component({
   selector: 'page-home',
@@ -11,6 +12,8 @@ export class HomePage {
   lastReleases:Array<Object> = [];
 
   constructor(public navCtrl: NavController, private nativeStorage : NativeStorage) {
+
+    this.getSpotifyAccessToken();
     var arrayTest =
     [
       {
@@ -33,4 +36,51 @@ export class HomePage {
     });
   }
 
+  getSpotifyAccessToken()
+  {
+    var client_id = "d9525608aeaf4de7bc7429483d04335d";
+    var client_secret = "1c2ca9c8e8bc47afb6209d19f6ea43cd";
+    var authorizationHeaderBase64 = btoa(client_id + ":" + client_secret);
+    debugger;
+    /*$.ajax({
+      method:"POST",
+      headers: {
+        "Authorization": "Basic " + authorizationHeaderBase64,
+        "Content-Type":"application/x-www-form-urlencoded",
+      },
+      url: "https://accounts.spotify.com/api/token",
+      dataType : "json",
+      data: {
+        client_id:"d9525608aeaf4de7bc7429483d04335d",
+        client_secret:"1c2ca9c8e8bc47afb6209d19f6ea43cd",
+        grant_type:"client_credentials",
+      },
+      error: function() {
+        $('#info').html('<p>An error has occurred</p>');
+      },
+      success: function(data){
+        console.log("Great ! " + JSON.stringify(data));
+        this.nativeStorage.setItem('accessToken', data.token_type + " " + data.access_token)
+      }
+    });*/
+    $.ajax({
+      method:"POST",
+      headers: {
+        "Authorization": "Basic " + authorizationHeaderBase64,
+        "Content-Type":"application/x-www-form-urlencoded",
+      },
+      url: "https://accounts.spotify.com/api/token",
+      dataType : "json",
+      data: {
+        client_id:"d9525608aeaf4de7bc7429483d04335d",
+        client_secret:"1c2ca9c8e8bc47afb6209d19f6ea43cd",
+        grant_type:"client_credentials",
+      }
+    }).then((data) => {
+      console.log("Great ! " + JSON.stringify(data));
+        this.nativeStorage.setItem('accessToken', data.token_type + " " + data.access_token);
+    },(error) => {
+      $('#info').html('<p>An error has occurred</p>');
+    });
+  }
 }
--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import * as $ from "jquery";
 
 @Component({
   selector: 'page-search',
@@ -8,6 +9,30 @@ import { NavController } from 'ionic-angular';
 export class SearchPage {
 
   constructor(public navCtrl: NavController) {
+
+    //Ajax Query
+
+    /*$.ajax({
+      url: "http://ws.audioscrobbler.com/2.0/",
+      method: "GET",
+      data: {
+        method:"artist.getinfo",
+        artist:"Cher",
+        format:"json",
+        api_key:"2a303e8f8744fc9fa899b12d331a7146"
+      },
+      error: function() {
+        $('#info').html('<p>An error has occurred</p>');
+      },
+      dataType: 'jsonp',
+      success: function(data){
+        console.log("Great ! " + JSON.stringify(data));
+      }, 
+      type:'GET'
+    });*/
+
+
+
 
   }
 
