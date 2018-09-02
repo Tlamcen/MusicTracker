139328bd657ca27a3b16e1aed77f600f412e7af1
Tlamcen <Tlamcen@users.noreply.github.com>
2018-09-02 11:27:41 +0200

    When the artist's search is completed, results are displayed under the searchbar

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -9,35 +9,57 @@ import * as $ from "jquery";
 })
 export class SearchPage {
 
-  accessToken:string = "";
+  accessToken : string = "";
+  artistsQueryResult : Object = {};
 
   constructor(public navCtrl: NavController, private nativeStorage : NativeStorage) {
     this.nativeStorage.getItem('accessToken').then((value) => {
       this.accessToken = value;
     });
   }
+
   onInput(event)
   {
-    console.log("onInput event triggered");
-    $.ajax({
-      method:"GET",
-      headers: {
-        "Authorization": this.accessToken,
-        "Content-Type":"application/json",
-      },
-      url: "https://api.spotify.com/v1/search",
-      dataType : "json",
-      data: {
-        q : "*" + event.target.value + "*",
-        type : "artist"
-      }
-    }).then((data) => {
-      console.log("Search completed ! " + data);
-    },(error) => {
-      $('#info').html('<p>An error has occurred</p>');
-    });
+    if(event.target.value){
+      console.log("onInput event triggered");
+      $.ajax({
+        method:"GET",
+        headers: {
+          "Authorization": this.accessToken,
+          "Content-Type":"application/json",
+        },
+        url: "https://api.spotify.com/v1/search",
+        dataType : "json",
+        data: {
+          q : "*" + event.target.value + "*",
+          type : "artist"
+        }
+      }).then((data) => {
+        console.log("Search completed ! " + data);
+        this.FillArtistsPageWithResults(data);
+      },(error) => {
+        $('#info').html('<p>An error has occurred</p>');
+      });
+    }
+    else
+    {
+      this.artistsQueryResult = {};
+    }
   }
+
   onCancel(event){
     console.log("onCancel event triggered");
+  }
+
+  FillArtistsPageWithResults(data)
+  {
+    if(data && data.artists && data.artists.items.length > 0)
+    {
+      this.artistsQueryResult = data;
+    }
+    else
+    {
+      this.artistsQueryResult = {};
+    }
   }
 }
